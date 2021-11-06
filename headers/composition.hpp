fb48af54e437f9b309b1c4a6a9b7814e653e0947
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-11-06 23:15:49 +0000

    Add composition

---
+++ headers/composition.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <array>
+#include <functional>
+
+template<unsigned SIZE1, unsigned SIZE2>
+using fn = std::function<std::array<unsigned, SIZE2>(std::array<unsigned, SIZE1>)>;
+
+template<unsigned SIZE1, unsigned SIZE2, unsigned SIZE3>
+fn<SIZE1, SIZE3> composition(
+    const fn<SIZE1, SIZE2>& function1,
+    const fn<SIZE2, SIZE3>& function2) noexcept {
+    return [&]
+        (const std::array<unsigned, SIZE1>& array)
+        -> std::array<unsigned, SIZE3> {
+        return function2(function1(array));
+    };
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,35 @@
 #include <array>
 
 #include "headers/combination.hpp"
+#include "headers/composition.hpp"
 
 using std::cout;
 
+std::array<unsigned, 5> bigger(std::array<unsigned, 2> array) {
+    std::array<unsigned, 5> result;
+    for (auto i = 0; i < 2; i++) {
+        result[i] = array[i];
+    }
+    for (auto i = 0; i < 3; i++) {
+        result[i + 2] = i;
+    }
+    return result;
+}
+
+std::array<unsigned, 3> smaller(std::array<unsigned, 5> array) {
+    std::array<unsigned, 3> result;
+    for (auto i = 0; i < 3; i++) {
+        result[i] = array[i];
+    }
+    return result;
+}
+
 int main() {
-    std::array<unsigned, 3> array1 = {1, 2, 3};
     std::array<unsigned, 2> array2 = {2, 3};
-    std::array<unsigned, 5> array3 = combination<3, 2>(array1, array2);
+    auto bigger_fn = bigger;
+    auto smaller_fn = smaller;
+    auto function = composition<2, 5, 3>(bigger_fn, smaller_fn);
+    auto array3 = function(array2);
 
     for (auto n : array3) {
         cout << n << std::endl;
