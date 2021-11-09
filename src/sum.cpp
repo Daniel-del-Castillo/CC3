6928000e69a88653f29ebb4a41eba5f2bbcf559e
Daniel-del-Castillo <alu0101225548@ull.edu.es>
2021-11-09 18:21:29 +0000

    Fix the sum not working properly

--- src/sum.cpp
+++ src/sum.cpp
@@ -3,15 +3,11 @@
 std::array<unsigned, 1> Sum::recursion_base_case(
     const std::array<unsigned, 1>& array
 ) const  {
-    return composition<1, 1, 1>(
-        successor,
-        composition<1, 1, 1>(zero, projection<1, 1>)
-    )(array);
+    return projection<1, 1>(array);
 }
 
 std::array<unsigned, 1> Sum::recursion(
     const std::array<unsigned, 3>& array
 ) const {
-    // Succesor function missing
-    return projection<3, 3>(array);
+    return composition<3, 1, 1>(successor, projection<3, 3>)(array);
 }
